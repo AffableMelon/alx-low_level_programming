90e465faf6e5027b55550885e0032c986239342c
Marshy <AffableMelon@gmail.com>
2022-12-13 14:14:29 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _abs - returns absolute value of the number
+ * @n: the number
+ * Return positive number only
+ */
+int print_sign(int)
+	int n;
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		n*(-1)
+		return (n);
+	}
+}
