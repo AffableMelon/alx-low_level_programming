75232f1fca93f2a7a6b2ea17496927a6dbcace3d
Marshy <AffableMelon@gmail.com>
2022-12-13 11:25:46 -0800

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - print _putchar and new line
  * Return: 0 it it exists
  */
 int main(void)
 {
-	char p = '_';
-	char a = 'p';
-	char b = 'u';
-	char c = 't';
-	char d = 'c';
-	char e = 'h';
-	char f = 'a';
-	char g = 'r';
-	char h = '\n';
-
-	putchar(p);
-	putchar(a);
-	putchar(b);
-	putchar(c);
-	putchar(d);
-	putchar(e);
-	putchar(f);
-	putchar(g);
-	putchar(h);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
