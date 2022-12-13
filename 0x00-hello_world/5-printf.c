db09796a0535bf59eee5a896cc7c79601d88ca0a
Marshy <AffableMelon@gmail.com>
2022-12-13 04:56:29 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the given word and a new line
+ * Return: 0 (true)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
