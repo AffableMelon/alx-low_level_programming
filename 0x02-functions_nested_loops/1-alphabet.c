558524376315d3bd6df2fb3d9f10e832be9f9651
Marshy <AffableMelon@gmail.com>
2022-12-13 11:00:47 -0800

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	char n;
 
 	for (n = 'a'; n <= 'z'; n++)
 		putchar(n);
 	putchar('\n');
-	return (0);
 }
