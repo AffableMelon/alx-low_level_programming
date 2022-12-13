fe376026d2243d7a58bfc6e6201038fc57331f07
Marshy <AffableMelon@gmail.com>
2022-12-13 08:56:47 -0800

    i feel ike a fucking robot ahhh

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print _putchar and new line
+ * Return: 0 it it exists
+ */
+int main(void)
+{
+	char p = '_';
+	char a = 'p';
+	char b = 'u';
+	char c = 't';
+	char d = 'c';
+	char e = 'h';
+	char f = 'a';
+	char g = 'r';
+	char h = '\n';
+
+	putchar(p);
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	return (0);
+}
