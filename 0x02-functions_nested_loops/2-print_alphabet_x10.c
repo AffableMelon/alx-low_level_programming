b99e7670b9f13d52673349dd1d6b651d4c0282f7
Marshy <AffableMelon@gmail.com>
2022-12-13 12:23:48 -0800

    2 and main

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print alphaet 10 times.
+ *
+ */ 
+void print_alphabet_x10(void)
+{
+	char n;
+	int i = 0;
+
+	while(i < 10) 
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('\n');
+		i++;
+	}
+}
