9bb727546bd55dec1ca6911b7864783b6a42db63
Marshy <AffableMelon@gmail.com>
2022-12-13 14:04:21 -0800

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_sign - checks if a number is positive negatie or zero
+ * @n: the number
+ * Return: 1 if number is + 0 if number is 0 otherwise -1 if number is  negative
+ */
+int print_sign(int n)
+{
+	if (n>0)
+		return (1);
+	if (n<0)
+		return (-1);
+	else
+		return(0):
+}
