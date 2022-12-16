c3d0685b6d5de95dc18ab24917345b5882047d1e
Marshy <AffableMelon@gmail.com>
2022-12-16 13:10:53 -0800

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - prints numbers 0 to 9 without 2 and 4
+ *
+ * Retruns: returns 0-9 without 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
