f1b22d51fb2bdc1d54ca241d69f85e546bb9dd6c
Marshy <AffableMelon@gmail.com>
2022-12-19 11:54:25 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,7 @@ void print_rev(char *str)
 {
 	int i;
 
-	i = =
+	i = 0;
 	for (;*str != '\0';)
 	{
 		i++;
@@ -19,7 +19,7 @@ void print_rev(char *str)
 	}
 	while (i >= 0)
 	{
-		_putchar(str[i]);
+		_putchar(*str);
 		i--;
 		str--;
 	}
