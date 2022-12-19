3374284f2a78544c7db98b4109484a490f9f362f
Marshy <AffableMelon@gmail.com>
2022-12-19 09:18:45 -0800

    1+

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,14 @@
 #include "main.h"
 /**
  * swap_int - swaps the given ints
- * @a: first int 
+ * @a: first int
  * @b: second int
  * Return: swapped ints
  */
 void swap_int(int *a, int *b)
 {
 	int temp;
-	
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
