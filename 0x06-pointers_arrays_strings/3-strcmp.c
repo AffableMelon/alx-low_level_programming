c84e560a081b93f28b0e402f0c5c2c9f00debf86
Marshy <AffableMelon@gmail.com>
2022-12-22 12:29:41 -0800

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,29 +14,33 @@ int _strcmp(char *s1, char *s2)
 	int i, j;
 
 	i = 0;
-	if (s1[i] == '\0' && s2[i] == '\0')
+	j = 0;
+
+	while (i >= 0)
 	{
-		break;
-	}
-	else if (s1[i] == '\0')
-	{
-		j = s2[i];
-		break;
-	}
-	else if (s2[i] == '\0')
-	{
-		j = s1[i];
-		break;
-	}
-	else if (s1[i] != s2[i])
-	{
-		j = s1[i] - s2[i];
-		break;
-	}
-	else
-	{
-		i++;
+		if (s1[i] == '\0' && s2[i] == '\0')
+		{
+			break;
+		}
+		else if (s1[i] == '\0')
+		{
+			j = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			j = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			j = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			i++;
+		}
 	}
 	return (j);
 }
-
