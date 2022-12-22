f877587a7cd7ca62c92cf072b29dc77ca1a0983f
Marshy <AffableMelon@gmail.com>
2022-12-22 12:18:33 -0800

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - returns the difference between two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: the dif between the two
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	i = 0;
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		break;
+	}
+	else if (s1[i] == '\0')
+	{
+		j = s2[i];
+		break;
+	}
+	else if (s2[i] == '\0')
+	{
+		j = s1[i];
+		break;
+	}
+	else if (s1[i] != s2[i])
+	{
+		j = s1[i] - s2[i];
+		break;
+	}
+	else
+	{
+		i++;
+	}
+	return (j);
+}
+
