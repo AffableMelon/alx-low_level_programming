3672cfd908f1cb13e132d0d0631629dea2c5a6d7
Marshy <AffableMelon@gmail.com>
2023-01-03 09:50:17 -0800

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,17 +8,17 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int j, i;
+	int i, j;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		for (j=0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
-			{
-				return (needle);
-			}
+			if (haystack[i + j] != needle[j])
+				break;
 		}
+		if (!needle[j])
+			return (&haystack[i]);
 	}
 	return (NULL);
 }
