342f740d4396ed7f8871b1cdc7486c054fbd7ad8
Marshy <AffableMelon@gmail.com>
2023-01-03 00:20:05 -0800

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strspn - gives number of bytes in the initial str
+ * @s: pointer to be checked
+ * @accept: pointer to be found
+ * Return: bytes of appearance
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, match;
+
+	match = 0;
+	for ( i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				match++;
+			}
+			j++;
+		}
+	}
+	return (match);
+}
