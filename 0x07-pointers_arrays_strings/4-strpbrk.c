5e100d07639f88bab0154f5818fd6dfe29573ab2
Marshy <AffableMelon@gmail.com>
2023-01-03 03:10:55 -0800

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,10 +9,10 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int j;
-	
+
 	while (*s)
 	{
-		for (j = 0 ;accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (*s == accept[j])
 			{
