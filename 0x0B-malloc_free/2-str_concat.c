d006cf47e4415163ded2af6aa2de97a9dc9bf682
Marshy <AffableMelon@gmail.com>
2023-01-10 13:39:51 -0800

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,11 +28,11 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	for (k = 0; k < i; k++)
 		a[k] = s1[k];
-	
+
 	for (l = 0; l <= j; l++)
 	{
 		a[k] = s2[l];
 		k++;
 	}
 	return (a);
-}	
+}
